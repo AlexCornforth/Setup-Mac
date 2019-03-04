eb3c42f2150a703df7f585759c5dfc554f43f063
Alex Cornforth <alexcornforth@me.com>
2019-03-04 12:45:03 +0000

    Added script

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "*** Setting Up New Mac ***"
+
+#General
+
+#Brew
+#Ruby
+#NPM
+#Git
+#Chrome
+
+#iOS Development
+
+#Fastlane
+#Xcode
+#Cocoapods
+#Carthage
+#Swift Package Manager
+#Xcode command line Tools
+
+#General Development Tools
+
+#Charles
+#Postman
+#iTerm
+#IntelliJ
+#Android Studio
+#AWS command line tools
+#Auto
+#Visual Studio Code
+#SourceTree
+
+#Other Helpful Stuff
+#Autojump
