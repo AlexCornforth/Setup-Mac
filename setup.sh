ec9266c035e4472ae1186e98800329c8d2670145
Alex Cornforth <alexcornforth@me.com>
2019-03-11 18:41:39 +0000

    Added apps to setup script

--- setup.sh
+++ setup.sh
@@ -1,35 +1,100 @@
 #!/bin/bash
-
+echo ""
 echo "*** Setting Up New Mac ***"
-
-#General
+echo ""
+read -s -p "Enter Password for sudo: " sudoPW
+echo ""
 
 #Brew
-#Ruby
-#NPM
-#Git
-#Chrome
+echo ""
+echo "* Homebrew"
+if test ! $(which brew); then
+  echo "Installing Homebrew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew already installed"
+fi
 
-#iOS Development
+# Update homebrew recipes
+echo "Updating homebrew..."
+brew update
 
-#Fastlane
-#Xcode
+#Xcode command line Tools - This will also install git
+echo "Installing Xcode Command Line Tools..."
+xcode-select --install
+
 #Cocoapods
-#Carthage
-#Swift Package Manager
-#Xcode command line Tools
+echo ""
+echo "* Cocoapods"
+if test ! $(which cocoapods); then
+  echo "Installing cocoapods..."
+  echo $sudoPW | sudo gem install cocoapods
+else
+  echo "Cocoapods already installed"
+fi
 
-#General Development Tools
+#pip
+echo ""
+echo "* pip"
+if test ! $(which pip); then
+  echo "Installing pip..."
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  python get-pip.py
+else
+  echo "pip already installed"
+fi
 
-#Charles
-#Postman
-#iTerm
-#IntelliJ
-#Android Studio
-#AWS command line tools
-#Auto
-#Visual Studio Code
-#SourceTree
+#AWS
+echo ""
+echo "* AWS CLI"
+if test ! $(which aws); then
+  echo "Installing AWS CLI..."
+  pip install awscli
+else
+  echo "AWS CLI already installed"
+fi
 
-#Other Helpful Stuff
-#Autojump
+#Brew Packages
+declare -a brew_apps=(
+  node
+  carthage
+)
+
+for app in "${brew_apps[@]}"; do
+  echo ""
+  echo "* ${app}"
+  echo "Installing: ${app}..."
+  brew install ${app}
+done
+
+#Applications
+declare -a cask_apps=(
+  alfred
+  caffeine
+  dash
+  dropbox
+  evernote
+  google-chrome
+  iterm2
+  slack
+  sourcetree
+  spotify
+  visual-studio-code
+  fastlane
+  postman
+  charles
+  atom
+  intellij-idea-ce
+  android-studio
+  dropbox
+)
+
+for app in "${cask_apps[@]}"; do
+  echo ""
+  echo "* ${app}"
+  echo "Installing: ${app}..."
+  brew cask install --appdir="/Applications" ${app}
+done
+
+brew cleanup
+brew cask alfred link
